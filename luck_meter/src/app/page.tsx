f3728a5b0a0e8b7765dfc8ee316ad454a88955ad
nigaor <ktnkuhi0116@gmail.com>
2025-05-21 02:12:59 +0900

    5/21 commit

--- luck_meter/src/app/page.tsx
+++ luck_meter/src/app/page.tsx
@@ -106,7 +106,7 @@ const response = await ai.models.generateContent({
   model: "gemini-2.0-flash",
   contents: "Write a story about a magic backpack.",
 });
-console.log(response.text);
+console.log(response.text); 
 }
 
 
